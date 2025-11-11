fd6e336cc717692b44aab235d01c907c6c3f1aaf
Raunakaga2004 <raunakaga12@gmail.com>
2025-11-12 00:07:20 +0530

    final basic deploy

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000/api/v1";
+// export const BACKEND_URL = "http://localhost:3000/api/v1";
+export const BACKEND_URL = "https://second-brain-backend-4vxg.onrender.com/api/v1"
